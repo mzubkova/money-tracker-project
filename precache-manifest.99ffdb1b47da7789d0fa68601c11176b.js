e822ec92d0fddfe1394646b2abdaa59b93e668b7
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-09-16 14:42:53 +0300

    Updates

---
+++ precache-manifest.99ffdb1b47da7789d0fa68601c11176b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c3670c71467edc011a216f69b0c04c",
+    "url": "/money-tracker-project/index.html"
+  },
+  {
+    "revision": "80c21a83f99efa8ea89d",
+    "url": "/money-tracker-project/static/css/main.9207fa33.chunk.css"
+  },
+  {
+    "revision": "e4c930b52a293f671fae",
+    "url": "/money-tracker-project/static/js/2.8491a0cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/money-tracker-project/static/js/2.8491a0cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80c21a83f99efa8ea89d",
+    "url": "/money-tracker-project/static/js/main.ee08dadd.chunk.js"
+  },
+  {
+    "revision": "2d974a2db0920c431941",
+    "url": "/money-tracker-project/static/js/runtime-main.fb652fb3.js"
+  }
+]);
